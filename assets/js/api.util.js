7e7d60967e8799126b715ff3cc0686f47808834e
kahverengi <ahmet.duruer@yandex.com>
2019-04-22 16:17:20 +0200

    Added assets to vanilla theme

---
+++ assets/js/api.util.js
@@ -0,0 +1,47 @@
+const NETWORK_ERROR = "NETWORK_ERROR"
+
+const config = {
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+    }
+}
+
+const ApiUtil = {
+
+    init() {
+        axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+        axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+    },
+
+    get(resource) {
+        return axios.get(resource, config)
+    },
+
+    post(resource, data) {
+        return axios.post(resource, data, config)
+    },
+
+    put(resource, data) {
+        return axios.put(resource, data, config)
+    },
+
+    delete(resource) {
+        return axios.delete(resource, config)
+    },
+
+    /**
+     * Perform a custom Axios request.
+     *
+     * data is an object containing the following properties:
+     *  - method
+     *  - url
+     *  - data ... request payload
+     *  - auth (optional)
+     *    - username
+     *    - password
+     **/
+    customRequest(data) {
+        return axios(data)
+    }
+}
---
+++ assets/js/router.js
@@ -0,0 +1,20 @@
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        // {
+        //     path: '/',
+        //     name: 'Main',
+        //     component: function (resolve, reject) {
+        //         loadComponent('Main', '/components/Pages/Main').then(resolve, reject);
+        //     }
+        // }
+    ]
+});
+
+
+// router.beforeEach((to, from, next) => {
+// })
+
+// router.afterEach((to, from) => {
+//     store.state.routePageLoading = false
+// })
