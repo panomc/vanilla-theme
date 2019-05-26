be7dd9a3979d978f068de39dcaaa4a116b51297d
kahverengi <ahmet.duruer@yandex.com>
2019-05-26 16:03:08 +0200

    Optimized UI bootstraps

--- components/App/index.js
+++ components/App/index.js
@@ -12,55 +12,53 @@ function loadComponent(componentName, path) {
   });
 }
 
-requirejs(["/assets/js/router.js", "/assets/js/api.util.js", "/assets/js/storage.util.js", "/assets/js/vuex-store.js", "/assets/js/i18n.js"], function () {
-  ApiUtil.init()
+ApiUtil.init()
 
-  axios.get('/components/App/ui').then(function (response) {
-    const app = new Vue({
-      el: '#app',
-      template: response.data,
-      router,
-      i18n,
-      store,
-      metaInfo() {
-        return {
-          title: this.$t("Common.Page.title"),
-          meta: [
-            {name: 'description', content: this.$t("Common.Page.description")}
-          ]
-        }
-      },
-      methods: {
-        setLang(lang) {
-          this.$store.dispatch('setLang', lang)
-        }
-      },
-      computed: {
-        langLoading() {
-          return this.$store.state.langLoading
-        }
-      },
-      beforeMount() {
-        let loadLanguage;
-
-        if (LanguageUtil.isThereLanguage())
-          loadLanguage = LanguageUtil.getLanguage()
-        else if (navigator.language.toUpperCase() === "tr".toUpperCase() || navigator.language.toUpperCase() === "tr-tr".toUpperCase())
-          loadLanguage = "tr"
-        else
-          loadLanguage = "en"
-
-        loadLanguageAsync(loadLanguage)
-      },
-      mounted() {
-
-        // Enable Bootstrap 4 Tooltips
-
-        $(function () {
-          $('[data-toggle="tooltip"]').tooltip()
-        })
-
+axios.get('/components/App/ui').then(function (response) {
+  const app = new Vue({
+    el: '#app',
+    template: response.data,
+    router,
+    i18n,
+    store,
+    metaInfo() {
+      return {
+        title: this.$t("Common.Page.title"),
+        meta: [
+          {name: 'description', content: this.$t("Common.Page.description")}
+        ]
       }
-    });
+    },
+    methods: {
+      setLang(lang) {
+        this.$store.dispatch('setLang', lang)
+      }
+    },
+    computed: {
+      langLoading() {
+        return this.$store.state.langLoading
+      }
+    },
+    beforeMount() {
+      let loadLanguage;
+
+      if (LanguageUtil.isThereLanguage())
+        loadLanguage = LanguageUtil.getLanguage()
+      else if (navigator.language.toUpperCase() === "tr".toUpperCase() || navigator.language.toUpperCase() === "tr-tr".toUpperCase())
+        loadLanguage = "tr"
+      else
+        loadLanguage = "en"
+
+      loadLanguageAsync(loadLanguage)
+    },
+    mounted() {
+
+      // Enable Bootstrap 4 Tooltips
+
+      $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
+
+    }
   });
 });
