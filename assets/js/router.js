5987849e3783e9323b71d33aa331845faf3578c9
kahverengi <ahmet.duruer@yandex.com>
2019-06-14 17:15:29 +0300

    Changed structure

--- assets/js/router.js
+++ assets/js/router.js
@@ -5,21 +5,21 @@ const router = new VueRouter({
       path: '/',
       name: 'Main',
       component: function (resolve, reject) {
-        loadComponent('Main', '/components/Pages/Main').then(resolve, reject);
+        loadComponent('Main', '/src/pages/Main').then(resolve, reject);
       }
     },
     {
       path: '/post',
       name: 'Post',
       component: function (resolve, reject) {
-        loadComponent('Post', '/components/Pages/Post').then(resolve, reject);
+        loadComponent('Post', '/src/pages/Post').then(resolve, reject);
       }
     },
     {
       path: '/profile',
       name: 'Profile',
       component: function (resolve, reject) {
-        loadComponent('Profile', '/components/Pages/Profile').then(resolve, reject);
+        loadComponent('Profile', '/src/pages/Profile').then(resolve, reject);
       }
     }
   ]
