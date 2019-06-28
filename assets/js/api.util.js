b2b0aec5b99fab7a50d480adbbcf966498348527
kahverengi <ahmet.duruer@yandex.com>
2019-06-28 20:07:05 +0300

    Enchanted API util

--- assets/js/api.util.js
+++ assets/js/api.util.js
@@ -1,11 +1,36 @@
-const NETWORK_ERROR = "NETWORK_ERROR"
+const NETWORK_ERROR = "NETWORK_ERROR";
 
 const config = {
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
   }
-}
+};
+
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  error => {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+
+    console.log(error.config);
+  }
+);
 
 const ApiUtil = {
 
@@ -14,19 +39,19 @@ const ApiUtil = {
     axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
   },
 
-  get(resource) {
+  get(resource, config = this.config) {
     return axios.get(resource, config)
   },
 
-  post(resource, data) {
+  post(resource, data, config = this.config) {
     return axios.post(resource, data, config)
   },
 
-  put(resource, data) {
+  put(resource, data, config = this.config) {
     return axios.put(resource, data, config)
   },
 
-  delete(resource) {
+  delete(resource, config = this.config) {
     return axios.delete(resource, config)
   },
 
@@ -44,4 +69,4 @@ const ApiUtil = {
   customRequest(data) {
     return axios(data)
   }
-}
+};
