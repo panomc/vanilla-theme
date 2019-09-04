398ae4b59e51e85a2bdac5f59fbf1ef7c9c19ad0
kahverengi <ahmet.duruer@yandex.com>
2019-09-04 20:20:26 +0200

    Started the showing first UI

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,51 +12,92 @@ function loadComponent(componentName, path) {
   });
 }
 
-ApiUtil.init()
+function LoadCSS(cssURL) {
 
-const app = new Vue({
-  el: '#app',
-  template: PANO.UI,
-  router,
-  i18n,
-  store,
-  metaInfo() {
-    return {
-      title: this.$t("Common.Page.title"),
-      meta: [
-        {name: 'description', content: this.$t("Common.Page.description")}
-      ]
-    }
-  },
-  methods: {
-    setLang(lang) {
-      this.$store.dispatch('setLang', lang)
-    }
-  },
-  computed: {
-    langLoading() {
-      return this.$store.state.langLoading
-    }
-  },
-  beforeMount() {
-    let loadLanguage;
+  // 'cssURL' is the stylesheet's URL, i.e. /css/styles.css
 
-    if (LanguageUtil.isThereLanguage())
-      loadLanguage = LanguageUtil.getLanguage()
-    else if (navigator.language.toUpperCase() === "tr".toUpperCase() || navigator.language.toUpperCase() === "tr-tr".toUpperCase())
-      loadLanguage = "tr"
-    else
-      loadLanguage = "en"
+  return new Promise(function (resolve, reject) {
 
-    loadLanguageAsync(loadLanguage)
-  },
-  mounted() {
+    const link = document.createElement('link');
 
-    // Enable Bootstrap 4 Tooltips
+    link.rel = 'stylesheet';
 
-    $(function () {
-      $('[data-toggle="tooltip"]').tooltip()
-    })
+    link.href = cssURL;
 
-  }
+    document.head.appendChild(link);
+
+    link.onload = function () {
+      resolve();
+    };
+  });
+}
+
+function LoadJS(jsURL) {
+
+  return new Promise(function (resolve, reject) {
+
+    const link = document.createElement('script');
+
+    link.src = jsURL;
+
+    document.head.appendChild(link);
+
+    link.onload = function () {
+      resolve();
+    };
+  });
+}
+
+requirejs(["/assets/js/router.js", "/assets/js/api.util.js", "/assets/js/storage.util.js", "/assets/js/vuex-store.js", "/assets/js/i18n.js"], function () {
+  LoadCSS('/assets/css/style.css').then(function () {
+
+    ApiUtil.init()
+
+    const app = new Vue({
+      el: '#app',
+      template: PANO.UI,
+      router,
+      i18n,
+      store,
+      metaInfo() {
+        return {
+          title: this.$t("Common.Page.title"),
+          meta: [
+            {name: 'description', content: this.$t("Common.Page.description")}
+          ]
+        }
+      },
+      methods: {
+        setLang(lang) {
+          this.$store.dispatch('setLang', lang)
+        }
+      },
+      computed: {
+        langLoading() {
+          return this.$store.state.langLoading
+        }
+      },
+      beforeMount() {
+        let loadLanguage;
+
+        if (LanguageUtil.isThereLanguage())
+          loadLanguage = LanguageUtil.getLanguage()
+        else if (navigator.language.toUpperCase() === "tr".toUpperCase() || navigator.language.toUpperCase() === "tr-tr".toUpperCase())
+          loadLanguage = "tr"
+        else
+          loadLanguage = "en"
+
+        loadLanguageAsync(loadLanguage)
+      },
+      mounted() {
+
+        // Enable Bootstrap 4 Tooltips
+
+        $(function () {
+          $('[data-toggle="tooltip"]').tooltip()
+        })
+
+      }
+    });
+  });
 });
