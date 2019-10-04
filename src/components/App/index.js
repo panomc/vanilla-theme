a2a999db51f411745e1174ee2cf43f74a10a624a
kahverengi <ahmet.duruer@yandex.com>
2019-10-04 10:14:03 +0200

    Fixed dependency loading with queue

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -13,9 +13,6 @@ function loadComponent(componentName, path) {
 }
 
 function LoadCSS(cssURL) {
-
-  // 'cssURL' is the stylesheet's URL, i.e. /css/styles.css
-
   return new Promise(function (resolve, reject) {
 
     const link = document.createElement('link');
@@ -32,89 +29,72 @@ function LoadCSS(cssURL) {
   });
 }
 
-function LoadJS(jsURL) {
-
-  return new Promise(function (resolve, reject) {
-
-    const link = document.createElement('script');
-
-    link.src = jsURL;
-
-    document.head.appendChild(link);
-
-    link.onload = function () {
-      resolve();
-    };
+loadMainJSFiles(function () {
+  requirejs.config({
+    paths: {
+      'router': '/assets/js/router',
+      'api-util': '/assets/js/api.util',
+      'storage-util': '/assets/js/storage.util',
+      'vuex-store': '/assets/js/vuex-store',
+      'i18n': '/assets/js/i18n'
+    },
+    shim: {
+      'api-util': ['router'],
+      'storage-util': ['api-util'],
+      'vuex-store': ['storage-util'],
+      'i18n': ['vuex-store']
+    }
   });
-}
 
-requirejs.config({
-  paths: {
-    'router': '/assets/js/router',
-    'api-util': '/assets/js/api.util',
-    'storage-util': '/assets/js/storage.util',
-    'vuex-store': '/assets/js/vuex-store',
-    'i18n': '/assets/js/i18n'
-  },
-  shim: {
-    'api-util': ['router'],
-    'storage-util': ['api-util'],
-    'vuex-store': ['storage-util'],
-    'i18n': ['vuex-store'],
-    'app-dependencies': ['i18n']
-  }
-});
+  requirejs(["i18n"], function () {
+    LoadCSS('/assets/css/style.css').then(function () {
+      ApiUtil.init();
 
-requirejs(["/assets/js/router.js", "/assets/js/api.util.js", "/assets/js/storage.util.js", "/assets/js/vuex-store.js", "/assets/js/i18n.js"], function () {
-  LoadCSS('/assets/css/style.css').then(function () {
+      const app = new Vue({
+        el: '#app',
+        template: PANO.UI,
+        router,
+        i18n,
+        store,
+        metaInfo() {
+          return {
+            title: this.$t("Common.Page.title"),
+            meta: [
+              {name: 'description', content: this.$t("Common.Page.description")}
+            ]
+          }
+        },
+        methods: {
+          setLang(lang) {
+            this.$store.dispatch('setLang', lang)
+          }
+        },
+        computed: {
+          langLoading() {
+            return this.$store.state.langLoading
+          }
+        },
+        beforeMount() {
+          let loadLanguage;
 
-    ApiUtil.init()
+          if (LanguageUtil.isThereLanguage())
+            loadLanguage = LanguageUtil.getLanguage()
+          else if (navigator.language.toUpperCase() === "tr".toUpperCase() || navigator.language.toUpperCase() === "tr-tr".toUpperCase())
+            loadLanguage = "tr"
+          else
+            loadLanguage = "en"
 
-    const app = new Vue({
-      el: '#app',
-      template: PANO.UI,
-      router,
-      i18n,
-      store,
-      metaInfo() {
-        return {
-          title: this.$t("Common.Page.title"),
-          meta: [
-            {name: 'description', content: this.$t("Common.Page.description")}
-          ]
+          loadLanguageAsync(loadLanguage)
+        },
+        mounted() {
+
+          // Enable Bootstrap 4 Tooltips
+
+          $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+          })
         }
-      },
-      methods: {
-        setLang(lang) {
-          this.$store.dispatch('setLang', lang)
-        }
-      },
-      computed: {
-        langLoading() {
-          return this.$store.state.langLoading
-        }
-      },
-      beforeMount() {
-        let loadLanguage;
-
-        if (LanguageUtil.isThereLanguage())
-          loadLanguage = LanguageUtil.getLanguage()
-        else if (navigator.language.toUpperCase() === "tr".toUpperCase() || navigator.language.toUpperCase() === "tr-tr".toUpperCase())
-          loadLanguage = "tr"
-        else
-          loadLanguage = "en"
-
-        loadLanguageAsync(loadLanguage)
-      },
-      mounted() {
-
-        // Enable Bootstrap 4 Tooltips
-
-        $(function () {
-          $('[data-toggle="tooltip"]').tooltip()
-        })
-
-      }
+      });
     });
   });
 });
