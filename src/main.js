6bb84a2460cef9e7332a4dc619c256e06b14fd68
kahverengi <ahmet.duruer@yandex.com>
2021-03-29 18:22:40 +0300

    Remove bootstrap from main.js and remove duplicate js utils from util folder

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
-import jquery from "jquery";
-import Popper from "popper.js";
 import "bootstrap";
 
-import { ApiUtil } from "./util/api.util";
+import { ApiUtil } from "./pano/js/api.util";
 
 import App from "./App.svelte";
 
 ApiUtil.init(process.env.API_URL);
-
-window.jQuery = window.jquery = window.$ = jquery;
-window.Popper = Popper;
 
 const app = new App({
   target: document.body,
