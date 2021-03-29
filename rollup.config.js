f65f7acb37f399ea6bdbdf92324235fbb8a95128
kahverengi <ahmet.duruer@yandex.com>
2021-03-29 19:52:36 +0300

    Rename folders in rollup.config and main.js

--- rollup.config.js
+++ rollup.config.js
@@ -13,8 +13,8 @@ import fs from "fs";
 fs.rmdirSync("public/assets", { recursive: true });
 fs.rmdirSync("public/commons", { recursive: true });
 
-fsExtra.copySync("src/pano/favicon", "public/commons/favicon");
-fsExtra.copySync("src/pano/fonts", "public/commons/fonts");
+fsExtra.copySync("src/pano-ui/favicon", "public/commons/favicon");
+fsExtra.copySync("src/pano-ui/fonts", "public/commons/fonts");
 fsExtra.copySync("src/assets", "public/assets");
 
 const configPath = "./config.js";
