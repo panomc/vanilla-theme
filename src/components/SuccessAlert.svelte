ea0fa330eea60539fc5f323cfa0747110c413555
kahverengi <ahmet.duruer@yandex.com>
2021-04-20 23:42:23 +0300

    Add SuccessAlert

---
+++ src/components/SuccessAlert.svelte
@@ -0,0 +1,42 @@
+<div
+  id="{id}"
+  class="alert border border-success text-success"
+  style="display: none;"
+  role="alert">
+  <div class="d-flex">
+    <div class="alert__icon mr-3">
+      <i class="fas fa-exclamation-circle"></i>
+    </div>
+
+    <div class="align-self-center mr-3">
+      {$message}
+    </div>
+
+    <div class="ml-auto">
+      <button
+        type="button"
+        class="alert__close alert__close--light"
+        aria-label="Close"
+        on:click="{hide}">
+        <span aria-hidden="true" class="text-success">×</span>
+      </button>
+    </div>
+  </div>
+</div>
+
+<script context="module">
+  import { writable } from "svelte/store";
+
+  const id = "successAlert";
+  let message = writable("");
+
+  export function show(successCode) {
+    message.set(successCode);
+
+    window.$("#" + id).fadeIn("slow");
+  }
+
+  export function hide() {
+    window.$("#" + id).fadeOut("fast");
+  }
+</script>
