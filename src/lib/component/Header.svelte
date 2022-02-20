69024a4809461d52ec5112228655c6fdbede0ef1
Selim Gökçek <selimgokcek@outlook.com>
2022-02-20 03:58:27 +0300

    Update header section

--- src/lib/component/Header.svelte
+++ src/lib/component/Header.svelte
@@ -1,30 +1,14 @@
 <!-- Header -->
 <header id="header">
   <div class="container">
-    <div class="row justify-content-center align-items-center">
-      <div class="col-12">
-        <a href="/">
-          <img
-            alt="Server Logo"
-            class="img-fluid d-block m-auto"
-            src="/assets/img/logo.png"
-            width="400"
-          />
-        </a>
-      </div>
-      <div class="col-12 pt-3">
-        <button
-          class="btn btn-sm btn-yellow shadow-yellow py-2 my-2 d-lg-none d-block
-          m-auto"
-          type="button"
-        >
-          <p class="text-neon text-white font-weight-bolder m-0">
-            OYNA.IPADRESI.COM
-          </p>
-          <small class="text-muted">IP adresini kopyalamak için tıkla!</small>
-        </button>
-      </div>
-    </div>
+    <a href="/">
+      <img
+        alt="Server Logo"
+        class="img-fluid d-block m-auto"
+        src="/assets/img/logo.png"
+        width="400" />
+    </a>
   </div>
 </header>
 <!-- Header End -->
+  
