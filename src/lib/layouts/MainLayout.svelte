d6b7792c557b02dd763a614f66f7e106800518cb
kahverengi <ahmet.duruer@yandex.com>
2022-02-21 22:16:12 +0300

    Move main layout under lib/layouts

---
+++ src/lib/layouts/MainLayout.svelte
@@ -0,0 +1,37 @@
+<Header />
+
+<Navbar />
+
+<Main>
+  <slot />
+</Main>
+
+<Footer />
+
+<!-- Modals Start-->
+<LoginModal />
+
+<ResetPasswordModal />
+
+<RegisterModal />
+
+<ForgottenPasswordModal />
+
+<!-- Modals End -->
+<script>
+  import { browser } from "$app/env";
+
+  if (browser) {
+    import("$lib/init.libs");
+  }
+
+  import Header from "$lib/component/Header.svelte";
+  import Navbar from "$lib/component/Navbar.svelte";
+  import Main from "$lib/component/Main.svelte";
+  import Footer from "$lib/component/Footer.svelte";
+
+  import LoginModal from "$lib/component/modals/LoginModal.svelte";
+  import ForgottenPasswordModal from "$lib/component/modals/ForgottenPasswordModal.svelte";
+  import RegisterModal from "$lib/component/modals/RegisterModal.svelte";
+  import ResetPasswordModal from "$lib/component/modals/ResetPasswordModal.svelte";
+</script>
