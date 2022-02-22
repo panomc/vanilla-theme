b4fa6a42005a1255a7f38ccf049edf7cfaa02ec3
kahverengi <ahmet.duruer@yandex.com>
2022-02-22 16:18:07 +0300

    Add ticket detail page

--- src/lib/pages/Tickets.svelte
+++ src/lib/pages/Tickets.svelte
@@ -1,1 +1,6 @@
 Tickets page
+
+<br />
+<br />
+
+<a href="/tickets/detail/10">Ticket Detail</a>
---
+++ src/routes/tickets/detail/[id].svelte
@@ -0,0 +1,5 @@
+<TicketDetail />
+
+<script>
+  import TicketDetail from "$lib/pages/tickets/TicketDetail.svelte";
+</script>
