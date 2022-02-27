63e650909035508f4ef35df756cba7284a9034a4
kahverengi <ahmet.duruer@yandex.com>
2022-02-27 23:03:27 +0300

    Remove jQuery and popper.js

--- src/lib/init.libs.js
+++ src/lib/init.libs.js
@@ -1,7 +1,3 @@
-import jquery from "jquery";
-import Popper from "popper.js";
 import * as bootstrap from "bootstrap";
 
-window.Popper = Popper;
 window.bootstrap = bootstrap;
-window.jQuery = window.jquery = window.$ = jquery;
