04790ab2ae84c719a8a615a83e3d1f0f0ecff4e4
Selim Gökçek <selimgokcek@outlook.com>
2022-03-07 00:34:46 +0300

    Navbar: Add icons

--- src/lib/component/Navbar.svelte
+++ src/lib/component/Navbar.svelte
@@ -1,8 +1,15 @@
 <!-- Navbar -->
+<style>
+  @media (max-width: 991.98px) {
+    .rounded-pill {
+      border-radius: 0.3rem !important;
+    }
+  }
+</style>
 
 <div class="container">
   <nav
-    class="navbar navbar-expand-lg navbar-dark bg-primary rounded-pill shadow px-lg-2 px-4">
+    class="navbar navbar-expand-lg navbar-dark bg-primary rounded-pill shadow px-lg-2 px-4 py-2">
     <button
       class="navbar-toggler d-lg-none"
       data-bs-target="#navbar"
@@ -21,11 +28,11 @@
               data-bs-toggle="dropdown"
               aria-haspopup="true"
               aria-expanded="false">
+              <i class="fas fa-user-circle"></i>
               {$session.user.username}
             </a>
-            <div class="dropdown-menu dropdown-menu-end">
+            <div class="dropdown-menu dropdown-menu-end position-absolute">
               <a class="dropdown-item" href="/profile">Profil</a>
-              <a class="dropdown-item" href="/tickets">Talepler</a>
               <a class="dropdown-item" href="/profile/settings">Ayarlar</a>
               <a
                 class="dropdown-item text-danger"
@@ -57,16 +64,12 @@
     <div class="collapse navbar-collapse" id="navbar">
       <ul class="navbar-nav text-lg-left text-center mr-auto mt-2 mt-lg-0">
         <li class="nav-item rounded-pill active">
-          <a class="nav-link" href="/">Ana Sayfa</a>
+          <a class="nav-link" href="/">
+            <i class="fas fa-home"></i>
+          </a>
         </li>
         <li class="nav-item rounded-pill">
-          <a class="nav-link" href="/">Ana Sayfa</a>
-        </li>
-        <li class="nav-item rounded-pill">
-          <a class="nav-link" href="/">Ana Sayfa</a>
-        </li>
-        <li class="nav-item rounded-pill">
-          <a class="nav-link" href="/">Ana Sayfa</a>
+          <a href="/tickets" class="nav-link">Destek</a>
         </li>
       </ul>
     </div>
