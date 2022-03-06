af64968aae9caa5b8fe24a5d0f4ca0aecda2de27
Selim Gökçek <selimgokcek@outlook.com>
2022-03-07 00:18:42 +0300

    Design TicketCreate page

--- src/lib/pages/ticket/TicketCreate.svelte
+++ src/lib/pages/ticket/TicketCreate.svelte
@@ -1,1 +1,33 @@
-Create a ticket!
+<div class="card">
+  <div class="card-body">
+    <div class="row justify-content-between mb-3">
+      <div class="col-auto">
+        <h4 class="card-title">Destek Talebi Oluştur</h4>
+      </div>
+      <div class="col-auto">
+        <a href="#">
+          <i class="fas fa-arrow-left mr-2"></i>
+          Talepler
+        </a>
+      </div>
+    </div>
+    <div class="mb-3">
+      <input
+        type="email"
+        class="form-control form-control-lg"
+        placeholder="Başlık" />
+    </div>
+    <input
+      class="form-control mb-3"
+      list="datalistOptions"
+      id="exampleDataList"
+      placeholder="Kategori" />
+    <datalist id="datalistOptions">
+      <option value="San Francisco"> </option><option value="New York">
+      </option><option value="Seattle"> </option><option value="Los Angeles">
+      </option><option value="Chicago"> </option></datalist>
+
+    <!-- Ticket Editor -->
+    <div class="mb-3">Editor</div>
+  </div>
+</div>
