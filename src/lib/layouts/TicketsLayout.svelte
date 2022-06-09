0abb41896b3fe25f10f814ad1fc96b4ae777ad84
kahverengi <ahmet@duruer.dev>
2022-06-09 14:32:04 +0200

    Add tickets page layout

---
+++ src/lib/layouts/TicketsLayout.svelte
@@ -0,0 +1,16 @@
+<!-- Tickets Page -->
+
+<slot />
+
+<script context="module">
+  export function load({ session: { user } }) {
+    if (!user) {
+      return {
+        status: 302,
+        redirect: "/",
+      };
+    }
+
+    return {};
+  }
+</script>
---
+++ src/routes/tickets/__layout.svelte
@@ -0,0 +1,27 @@
+<TicketsLayout>
+  <slot />
+</TicketsLayout>
+
+<script context="module">
+  import { load as loadLayout } from "$lib/layouts/TicketsLayout.svelte";
+
+  /**
+   * @type {import('@sveltejs/kit').Load}
+   */
+  export async function load(params) {
+    let output = {};
+
+    const data = await loadLayout(params);
+
+    output = {
+      ...output,
+      ...data,
+    };
+
+    return !!data ? output : null;
+  }
+</script>
+
+<script>
+  import TicketsLayout from "$lib/layouts/TicketsLayout.svelte";
+</script>
