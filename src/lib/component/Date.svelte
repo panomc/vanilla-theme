41717c22811836caf8bb76376290fe925dd8057a
kahverengi <ahmet@duruer.dev>
2022-06-10 22:54:56 +0200

    Improve Date component with adding format relative option

--- src/lib/component/Date.svelte
+++ src/lib/component/Date.svelte
@@ -2,15 +2,27 @@
   use:tooltip="{[
     format(new Date(parseInt(date)), 'dd/MM/yyyy, HH:mm'),
     { placement: 'bottom' },
-  ]}">{format(new Date(parseInt(date)), "dd MMMM yyyy")}</span
->
+  ]}">
+  <slot>
+    {#if relativeFormat}
+      {formatRelative(new Date(parseInt(date)), new Date()).capitalize()}
+    {:else}
+      {format(new Date(parseInt(date)), "dd MMMM yyyy")}
+    {/if}
+  </slot>
+</span>
 
 <script>
-  import { format } from "date-fns";
+  import { format, formatRelative } from "date-fns";
 
   import tooltip from "$lib/tooltip.util";
 
+  String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  };
+
   export let time;
+  export let relativeFormat = false;
 
   $: date = time;
 </script>
