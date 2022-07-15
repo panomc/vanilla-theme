60f1da562842ebb2aac12fc6ef8b4705ded36014
kahverengi <ahmet@duruer.dev>
2022-07-15 20:13:34 +0300

    Show category is not exists in TicketRow component

--- src/lib/component/TicketRow.svelte
+++ src/lib/component/TicketRow.svelte
@@ -9,28 +9,28 @@
         class="btn btn-sm btn-link link-bittersweet"
         role="button"
         href="javascript:void(0);"
-        on:click="{() => onCloseTicket()}"
-      >
+        on:click="{() => onCloseTicket()}">
         <i class="fas fa-check"></i>
       </a>
     {/if}
   </th>
   <td class="align-middle text-nowrap">
     <a href="/ticket/{ticket.id}" title="Talebi Görüntüle"
-      >#{ticket.id} {ticket.title}</a
-    >
+      >#{ticket.id} {ticket.title}</a>
   </td>
   <td class="align-middle text-nowrap">
     <TicketStatus status="{ticket.status}" />
   </td>
   <td class="align-middle text-nowrap">
-    <a use:tooltip="{['Filtrele', { placement: 'bottom' }]}" class="badge rounded-pill bg-light text-black" href="/tickets/category/{ticket.category.url}">
-      {ticket.category.title}
+    <a
+      use:tooltip="{['Filtrele', { placement: 'bottom' }]}"
+      class="badge rounded-pill bg-light text-black"
+      href="/tickets/category/{ticket.category.url}">
+      {ticket.category.title === "-" ? "Kategorisiz" : ticket.category.title}
     </a>
   </td>
   <td class="align-middle text-nowrap"
-    ><span><Date time="{ticket.lastUpdate}" /></span></td
-  >
+    ><span><Date time="{ticket.lastUpdate}" /></span></td>
 </tr>
 
 <script>
