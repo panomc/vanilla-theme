b9a3ee5f15713cdcdd11d61d17e1dbee859e712a
Selim Gökçek <selimgokcek@outlook.com>
2022-07-23 17:56:34 +0300

    Add panel link to navbar closes #19

--- src/lib/component/Navbar.svelte
+++ src/lib/component/Navbar.svelte
@@ -9,28 +9,32 @@
 
 <div class="container">
   <nav
-    class="navbar navbar-expand-lg navbar-dark fw-bolder-lightglass bg-primary bg-gradient rounded-pill shadow px-lg-2 px-4 py-2"
-  >
+    class="navbar navbar-expand-lg navbar-dark fw-bolder-lightglass bg-primary bg-gradient rounded-pill shadow px-lg-2 px-4 py-2">
     <button
       class="navbar-toggler d-lg-none"
       data-bs-target="#navbar"
       data-bs-toggle="collapse"
-      type="button"
-    >
+      type="button">
       <i aria-hidden="true" class="fa fa-bars"></i>
     </button>
 
     <ul class="navbar-nav flex-row ml-auto order-lg-last">
+      <li class="nav-item rounded-pill">
+        <a class="nav-link" href="/">
+          <i
+            class="fa-solid fa-arrow-up fa-rotate-by"
+            style="--fa-rotate-angle: 45deg;"></i> Panel
+        </a>
+      </li>
       {#if $session.user}
         <li class="nav-item rounded-pill mr-lg-0 mr-5">
           <div class="dropdown">
             <a
-              class="nav-link"
+              class="nav-link dropdown-toggle"
               href="javascript:void(0);"
               data-bs-toggle="dropdown"
               aria-haspopup="true"
-              aria-expanded="false"
-            >
+              aria-expanded="false">
               <i class="fas fa-user-circle"></i>
               {$session.user.username}
             </a>
@@ -40,8 +44,7 @@
               <a
                 class="dropdown-item text-danger"
                 href="javascript:void(0);"
-                on:click="{() => logout($session.CSRFToken)}">Çıkış Yap</a
-              >
+                on:click="{() => logout($session.CSRFToken)}">Çıkış Yap</a>
             </div>
           </div>
         </li>
@@ -50,8 +53,7 @@
           <a
             class="nav-link"
             href="javascript:void(0);"
-            on:click="{showLoginModal}"
-          >
+            on:click="{showLoginModal}">
             Giriş Yap
           </a>
         </li>
@@ -59,8 +61,7 @@
           <a
             class="nav-link"
             href="javascript:void(0);"
-            on:click="{showRegisterModal}"
-          >
+            on:click="{showRegisterModal}">
             Kayıt Ol
           </a>
         </li>
@@ -69,7 +70,7 @@
 
     <div class="collapse navbar-collapse" id="navbar">
       <ul class="navbar-nav text-lg-left text-center mr-auto mt-2 mt-lg-0">
-        <li class="nav-item rounded-pill active">
+        <li class="nav-item rounded-pill">
           <a class="nav-link" href="/">
             <i class="fa-solid fa-house"></i>
           </a>
