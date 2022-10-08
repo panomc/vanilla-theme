1eeaaabe48929a721670b6dc77ec305d43e5f75e
kahverengi <ahmet@duruer.dev>
2022-10-08 20:20:18 +0300

    Remove useless load codes from routes

--- src/routes/tickets/all/[page]/+page.js
+++ src/routes/tickets/all/[page]/+page.js
@@ -1,8 +1,1 @@
-import { load as loadTickets } from "$lib/pages/Tickets.svelte";
-
-/**
- * @type {import('@sveltejs/kit').PageLoad}
- */
-export async function load(event) {
-  return loadTickets(event);
-}
+export { load } from "$lib/pages/Tickets.svelte";
