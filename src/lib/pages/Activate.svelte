3460716802e37c6f562de0b493dd8f622c719be9
Selim Gökçek <selimgokcek@outlook.com>
2022-10-15 22:20:00 +0300

    Redesign activateion page

--- src/lib/pages/Activate.svelte
+++ src/lib/pages/Activate.svelte
@@ -1,4 +1,15 @@
-<button class="btn btn-success">Activate your e-mail</button>
+<div class="card">
+  <div class="card-body">
+    <h3>Verify your account</h3>
+    <div class="text-center">
+      <img
+        alt="Allay"
+        src="https://cdn3.emoji.gg/emojis/8182-allay-dancing.gif" />
+      <div class="d-block py-3">recaptcha</div>
+      <button class="btn btn-lg btn-secondary disabled">Activate E-mail</button>
+    </div>
+  </div>
+</div>
 
 <script context="module">
   /**
