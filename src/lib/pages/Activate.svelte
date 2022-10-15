4f0f63578836c876da477a109a7cac2a8695e9aa
kahverengi <ahmet@duruer.dev>
2022-10-15 20:40:40 +0300

    Add activate e-mail page

---
+++ src/lib/pages/Activate.svelte
@@ -0,0 +1,12 @@
+<button class="btn btn-success">Activate your e-mail</button>
+
+<script context="module">
+  /**
+   * @type {import('@sveltejs/kit').Load}
+   */
+  export async function load() {}
+</script>
+
+<script>
+  export let data;
+</script>
---
+++ src/routes/activate/+page.svelte
@@ -0,0 +1,7 @@
+<Activate data="{data}" />
+
+<script>
+  import Activate from "$lib/pages/Activate.svelte";
+
+  export let data;
+</script>
