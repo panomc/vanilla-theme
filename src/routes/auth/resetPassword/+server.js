c79e91a0358058f36453ff6409631c279632b7f1
kahverengi <ahmet@duruer.dev>
2022-10-16 15:44:11 +0300

    Fix not sending call request to reset password

--- src/routes/auth/resetPassword/+server.js
+++ src/routes/auth/resetPassword/+server.js
@@ -2,7 +2,7 @@ import * as api from "$lib/api-server.util";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function POST({ request }) {
-  const body = await api.POST("auth/forgotPassword", await request.text());
+  const body = await api.POST("auth/resetPassword", await request.text());
 
   return new Response(JSON.stringify(body));
 }
