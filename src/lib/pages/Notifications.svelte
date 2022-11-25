6b606bafa803c854905118b5af4ae50faa2e4a0c
kahverengi <ahmet@duruer.dev>
2022-11-26 01:29:08 +0300

    Add notifications page

---
+++ src/lib/pages/Notifications.svelte
@@ -0,0 +1,16 @@
+Bildirimler!!!
+
+
+<script context="module">
+  /**
+   * @type {import('@sveltejs/kit').Load}
+   */
+  export async function load(event) {
+    const { parent } = event;
+    await parent();
+  }
+</script>
+
+<script>
+  export let data;
+</script>
