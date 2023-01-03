a188fa67734438029ae65fbe4508d4e36dd9964f
kahverengi <ahmet@duruer.dev>
2023-01-03 21:44:52 +0300

    Make online admins component separate from sidebars

---
+++ src/lib/component/OnlineAdmins.svelte
@@ -0,0 +1,37 @@
+<!-- Online Admins -->
+<div class="card text-center mb-3">
+  <h5 class="card-header bg-sunflower text-white">Çevrimiçi Adminler</h5>
+  <div class="card-body p-0">
+    <table class="table table-borderless mb-0">
+      <tbody>
+        <tr>
+          <td>
+            <a href="/player/Butlu">
+              <img
+                alt=""
+                class="rounded"
+                src="https://crafthead.net/avatar/butlu"
+                use:tooltip="{['Butlu', { placement: 'bottom' }]}"
+                width="24" />
+            </a>
+          </td>
+          <td>
+            <a href="/player/kahverengi">
+              <img
+                alt=""
+                class="rounded"
+                src="https://crafthead.net/avatar/kahverengi"
+                use:tooltip="{['kahverengi', { placement: 'bottom' }]}"
+                width="24" />
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+
+<!-- Online Admins End -->
+<script>
+  import tooltip from "$lib/tooltip.util";
+</script>
--- src/lib/component/sidebars/TicketsSidebar.svelte
+++ src/lib/component/sidebars/TicketsSidebar.svelte
@@ -3,43 +3,12 @@
     <i class="fas fa-plus"></i>
     <br />Destek Talebi Oluştur
   </a>
-  <!-- Online Admins -->
-  <div class="card text-center mb-3">
-    <h5 class="card-header bg-sunflower text-white">Çevrimiçi Adminler</h5>
-    <div class="card-body p-0">
-      <table class="table table-borderless mb-0">
-        <tbody>
-          <tr>
-            <td>
-              <a href="/player/Butlu">
-                <img
-                  alt=""
-                  class="rounded"
-                  src="https://crafthead.net/avatar/butlu"
-                  use:tooltip="{['Butlu', { placement: 'bottom' }]}"
-                  width="24" />
-              </a>
-            </td>
-            <td>
-              <a href="/player/kahverengi">
-                <img
-                  alt=""
-                  class="rounded"
-                  src="https://crafthead.net/avatar/kahverengi"
-                  use:tooltip="{['kahverengi', { placement: 'bottom' }]}"
-                  width="24" />
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-  <!-- Online Admins End -->
+
+  <OnlineAdmins />
 </Sidebar>
 
 <script>
   import Sidebar from "$lib/component/Sidebar.svelte";
 
-  import tooltip from "$lib/tooltip.util";
+  import OnlineAdmins from "$lib/component/OnlineAdmins.svelte";
 </script>
