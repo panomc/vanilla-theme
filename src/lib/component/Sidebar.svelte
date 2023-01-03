79c5920bc54a3299ec014d51517749f87e24eab1
kahverengi <ahmet@duruer.dev>
2023-01-04 00:03:05 +0300

    Add sidebar side option

--- src/lib/component/Sidebar.svelte
+++ src/lib/component/Sidebar.svelte
@@ -1,5 +1,14 @@
 <!-- Sidebar -->
-<aside class="col-lg-4 order-lg-last order-first">
+<aside
+  class="col-lg-4 order-lg-last order-first"
+  class:order-lg-last="{side === 'right'}"
+  class:order-first="{side === 'right'}"
+  class:order-lg-first="{side === 'left'}"
+  class:order-last="{side === 'left'}">
   <slot />
 </aside>
+
 <!-- Sidebar End -->
+<script>
+  export let side = "right";
+</script>
