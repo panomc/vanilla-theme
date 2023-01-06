2e76644f25d4bbb5a4b11eb80038435d2127a5a9
Selim Gökçek <selimgokcek@tuta.io>
2023-01-06 14:55:28 +0300

    Make the online admins card to column

--- src/lib/component/OnlineAdmins.svelte
+++ src/lib/component/OnlineAdmins.svelte
@@ -2,24 +2,20 @@
 <div class="card text-center mb-3">
   <h5 class="card-header bg-sunflower text-white">Çevrimiçi Adminler</h5>
   <div class="card-body p-0">
-    <table class="table table-borderless mb-0">
-      <tbody>
-        <tr>
-          {#each onlineAdmins as onlineAdmin, index (onlineAdmin)}
-            <td>
-              <a href="/player/{onlineAdmin}">
-                <img
-                  alt=""
-                  class="rounded"
-                  src="https://crafthead.net/avatar/{onlineAdmin}"
-                  use:tooltip="{[onlineAdmin, { placement: 'bottom' }]}"
-                  width="24" />
-              </a>
-            </td>
-          {/each}
-        </tr>
-      </tbody>
-    </table>
+    <div class="row">
+      {#each onlineAdmins as onlineAdmin, index (onlineAdmin)}
+        <div class="col-3">
+          <a href="/player/{onlineAdmin}">
+            <img
+              alt=""
+              class="rounded m-2"
+              src="https://crafthead.net/avatar/{onlineAdmin}"
+              use:tooltip="{[onlineAdmin, { placement: 'bottom' }]}"
+              width="24" />
+          </a>
+        </div>
+      {/each}
+    </div>
   </div>
 </div>
 
