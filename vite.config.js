bed82323d0a5bb4815e37f2fdb0b6ec539f1adcb
kahverengi <ahmet@duruer.dev>
2023-01-14 23:36:17 +0300

    Add reverse proxy for be api in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8088'
+    }
+  }
 };
 
 export default config;
