a3cfb81e1212f4ffe5f5c20901cb65ec243aed00
kahverengi <ahmet@duruer.dev>
2023-10-11 22:13:35 +0300

    Fix title for website logo

--- src/lib/component/Header.svelte
+++ src/lib/component/Header.svelte
@@ -1,9 +1,15 @@
 <!-- Header -->
 <header id="header">
   <img
-    title="Panocraft"
+    title="{$session.siteInfo.websiteName}"
     alt="Server Logo"
     class="d-block m-auto p-3"
     src="/api/websiteLogo" />
 </header>
 <!-- Header End -->
+
+<script>
+  import { getContext } from "svelte";
+
+  const session = getContext("session");
+</script>
