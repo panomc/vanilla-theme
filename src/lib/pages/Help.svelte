107b8f09c5708dddf74f3278be0dcba771ce1790
Selim Gökçek <selimgokcek@outlook.com>
2023-10-12 15:29:24 +0300

    Seperate Support & Help page

---
+++ src/lib/pages/Help.svelte
@@ -0,0 +1,44 @@
+<div class="card bg-white">
+  <div class="card-body">
+    <h3 class="card-title">Yardım Merkezi</h3>
+    <p class="my-3">Nasıl yardım almak istersin?</p>
+    <ul class="list-group list-group-horizontal text-center">
+      <a
+        href="/ticket/create"
+        class="list-group-item list-group-item-action disabled">
+        <i class="fas fa-ticket fa-2x my-3"></i>
+        <h5>Oyun Rehberi</h5>
+        <p class="mb-0">
+          {$session.siteInfo.websiteName} öğretici rehberine göz at.
+        </p>
+      </a>
+
+      <a href="#" class="list-group-item list-group-item-action disabled">
+        <i class="fas fa-envelope fa-2x my-3"></i>
+        <div class="col-auto">
+          <h5>Kurallar</h5>
+          <p class="mb-0">Şartları ve kuralları öğren.</p>
+        </div>
+      </a>
+
+      <a href="#" class="list-group-item list-group-item-action disabled">
+        <i class="fas fa-question fa-2x my-3"></i>
+        <div class="col-auto">
+          <h5>Sık Sorulan Sorular</h5>
+          <p class="mb-0">
+            {$session.siteInfo.websiteName} hakkında aklına takılan sorulara cevap
+            bul.
+          </p>
+        </div>
+      </a>
+    </ul>
+  </div>
+</div>
+
+<script>
+  import { getContext } from "svelte";
+
+  export let data;
+
+  const session = getContext("session");
+</script>
--- src/lib/pages/Support.svelte
+++ src/lib/pages/Support.svelte
@@ -1,34 +1,21 @@
 <div class="card bg-white">
   <div class="card-body">
     <h3 class="card-title">Destek</h3>
-    <p class="my-3">Nasıl yardım almak istersin?</p>
+    <p class="my-3">Nasıl destek almak istersin?</p>
     <ul class="list-group list-group-horizontal text-center">
-      <a
-        href="/ticket/create"
-        class="list-group-item list-group-item-action disabled">
+      <a href="/ticket/create" class="list-group-item list-group-item-action">
         <i class="fas fa-ticket fa-2x my-3"></i>
-        <h5>Oyun Rehberi</h5>
+        <h5>Destek Talebi Oluştur</h5>
         <p class="mb-0">
-          {$session.siteInfo.websiteName} öğretici rehberine göz at.
+          Oyuncu olarak yetkililere mesaj gönder ve yanıtlandığında bildirim al.
         </p>
       </a>
 
-      <a href="#" class="list-group-item list-group-item-action disabled">
+      <a href="#" class="list-group-item list-group-item-action">
         <i class="fas fa-envelope fa-2x my-3"></i>
         <div class="col-auto">
-          <h5>Kurallar</h5>
-          <p class="mb-0">Şartları ve kuralları öğren.</p>
-        </div>
-      </a>
-
-      <a href="#" class="list-group-item list-group-item-action disabled">
-        <i class="fas fa-question fa-2x my-3"></i>
-        <div class="col-auto">
-          <h5>Sık Sorulan Sorular</h5>
-          <p class="mb-0">
-            {$session.siteInfo.websiteName} hakkında aklına takılan sorulara cevap
-            bul.
-          </p>
+          <h5>E-Posta Gönder</h5>
+          <p class="mb-0">Çeşitli konular için {$session.siteInfo.websiteName} yöneticisine e-posta gönder.</p>
         </div>
       </a>
     </ul>
